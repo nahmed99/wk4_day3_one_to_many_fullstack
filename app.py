e6366a39235907d64022f7b5e057ba5d034b84a2
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-11-25 18:58:18 +0000

    books index and delete working

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 
 # TODO: import books blueprint here
+from controllers.books_controller import books_blueprint
 
 app = Flask(__name__)
 
 # TODO: register books blueprint here
+app.register_blueprint(books_blueprint)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- controllers/books_controller.py
+++ controllers/books_controller.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, Blueprint, redirect, request
 from models.book import Book
 import repositories.book_repository as book_repository
 import repositories.author_repository as author_repository
 
+
+books_blueprint = Blueprint("books", __name__)
+
+
+# Also an INDEX - just in case user doesn't type in /books...
+@books_blueprint.route('/')
+def index():
+    # This will redirect the user to the books index page
+    return redirect("/books")
+
 # INDEX
 # GET '/books'
+@books_blueprint.route("/books")
+def books():
+    books = book_repository.select_all()
+    authors = author_repository.select_all
+    return render_template("books/index.html", all_books=books, all_authors=authors)
+
 
 # NEW
 # GET '/books/new'
@@ -29,4 +45,9 @@ import repositories.author_repository as author_repository
 
 # DELETE
 # DELETE '/books/<id>'
+
+@books_blueprint.route("/books/<id>/delete", methods=["POST"])
+def delete_book(id):
+    book_repository.delete(id)
+    return redirect('/books')
 
