4fe8dfd7ce601ca7c1f3808a43241b12cdcc2590
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-11-25 19:40:31 +0000

    Done for the day - extensions completed and tested

--- controllers/books_controller.py
+++ controllers/books_controller.py
@@ -24,14 +24,42 @@ def books():
 
 # NEW
 # GET '/books/new'
+@books_blueprint.route("/books/new", methods=["GET"])
+def new_book():
+    authors = author_repository.select_all()
+    # pass the authors, so that user can select the author name...or do we want free-form text (for a new author)???
+    return render_template("books/new.html", authors_list=authors)
 
 
 # CREATE
 # POST '/books'
+@books_blueprint.route("/books", methods=["POST"])
+def create_book():
+    # grab the form data for the the book variables: title, genre, publisher, author
+    title = request.form['title']
+    genre = request.form['genre']
+    publisher = request.form['publisher']
+    author_id = request.form['author_id']
+
+    # select (existing author)
+    author = author_repository.select(author_id)
+
+    # create new book object
+    book = Book(title, genre, publisher, author)
+
+    # Save new book object in database
+    book_repository.save(book)
+
+    return redirect('/books')
+
 
 
 # SHOW
 # GET '/books/<id>'
+@books_blueprint.route('/books/<id>')
+def show_book(id):
+    book = book_repository.select(id)
+    return render_template("books/show.html", book=book)
 
 
 # EDIT
